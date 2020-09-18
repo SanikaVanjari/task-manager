8734d2825b467a0045c96d04c1ffd21928602e30
Sanika Vanjari <sanika.vanjari@finshell.co.in>
2020-09-18 14:34:51 +0530

    no tasks display condition

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -6,11 +6,15 @@ function TaskList() {
   const { tasks } = useContext(TaskListContext)
   return (
     <div>
-      <ul className="list">
-        {tasks.map((task) => (
-          <Task key={task.id} task={task} />
-        ))}
-      </ul>
+      {tasks.length ? (
+        <ul className="list">
+          {tasks.map((task) => {
+            return <Task task={task} key={task.id} />
+          })}
+        </ul>
+      ) : (
+        <div className="no-tasks">No Tasks</div>
+      )}
     </div>
   )
 }
